553834e9163a21d49a8bc3ffe4a8040d9397be8c
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-06-03 07:49:03 +0700

    rewrite code Retrofit2

--- app/src/main/java/com/ahmadabuhasan/mypets_app/ApiClient.java
+++ app/src/main/java/com/ahmadabuhasan/mypets_app/ApiClient.java
@@ -1,5 +1,7 @@
 package com.ahmadabuhasan.mypets_app;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -9,19 +11,20 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
-    private static final String BASE_URL = "http://ipaddressServer/demo_pets/";
-    private static Retrofit retrofit;
+    private static final String BASE_URL = "https://blackpink-marketplace.000webhostapp.com/demo_pets/";
 
-    static Retrofit getApiClient() {
-
-        if (retrofit == null) {
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(new NullOnEmptyConverterFactory())
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
-
-        }
-        return retrofit;
+    public static ApiInterface getApiInterface() {
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor()
+                .setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build();
+        Retrofit retrofit = new retrofit2.Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(new NullOnEmptyConverterFactory())
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+        return retrofit.create(ApiInterface.class);
     }
 }
