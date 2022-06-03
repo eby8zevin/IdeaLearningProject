6ffa597dc806a287f9808767eeec1ddac6957fbb
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-06-03 07:46:31 +0700

    Maintenance Code (e.g viewBinding, etc)

--- app/src/main/java/com/ahmadabuhasan/mypets_app/Adapter.java
+++ app/src/main/java/com/ahmadabuhasan/mypets_app/Adapter.java
@@ -7,21 +7,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Filter;
 import android.widget.Filterable;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.ahmadabuhasan.mypets_app.databinding.ListItemBinding;
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import de.hdodenhof.circleimageview.CircleImageView;
 
 /*
  * Created by Ahmad Abu Hasan on 30/12/2020
@@ -30,8 +25,8 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> implements Filterable {
 
     List<Pets> pets, petsFilter;
-    private Context context;
-    private RecyclerViewClickListener mListener;
+    private final Context context;
+    final RecyclerViewClickListener mListener;
     CustomFilter filter;
 
     public Adapter(List<Pets> pets, Context context, RecyclerViewClickListener listener) {
@@ -41,40 +36,33 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> implemen
         this.mListener = listener;
     }
 
+    @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
-        return new MyViewHolder(view, mListener);
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ListItemBinding binding = ListItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new MyViewHolder(binding, null);
     }
 
     @SuppressLint("CheckResult")
     @Override
     public void onBindViewHolder(final MyViewHolder holder, int position) {
 
-        holder.mName.setText(pets.get(position).getName());
-        holder.mType.setText(pets.get(position).getBreed() + " / "
-                + pets.get(position).getSpecies());
-        holder.mDate.setText(pets.get(position).getBirth());
-
-        RequestOptions requestOptions = new RequestOptions();
-        requestOptions.skipMemoryCache(true);
-        requestOptions.diskCacheStrategy(DiskCacheStrategy.NONE);
-        requestOptions.placeholder(R.drawable.logo);
-        requestOptions.error(R.drawable.logo);
+        holder.binding.name.setText(pets.get(position).getName());
+        holder.binding.type.setText(String.format("%s / %s", pets.get(position).getBreed(), pets.get(position).getSpecies()));
+        holder.binding.date.setText(pets.get(position).getBirth());
 
         Glide.with(context)
                 .load(pets.get(position).getPicture())
-                .apply(requestOptions)
-                .into(holder.mPicture);
+                .apply(RequestOptions.placeholderOf(R.drawable.ic_loading)
+                        .error(R.drawable.ic_error))
+                .into(holder.binding.picture);
 
         final Boolean love = pets.get(position).getLove();
-
         if (love) {
-            holder.mLove.setImageResource(R.drawable.likeon);
+            holder.binding.love.setImageResource(R.drawable.likeon);
         } else {
-            holder.mLove.setImageResource(R.drawable.likeof);
+            holder.binding.love.setImageResource(R.drawable.likeof);
         }
-
     }
 
     @Override
@@ -86,41 +74,32 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> implemen
     public Filter getFilter() {
         if (filter == null) {
             filter = new CustomFilter((ArrayList<Pets>) petsFilter, this);
-
         }
         return filter;
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        private RecyclerViewClickListener mListener;
-        private CircleImageView mPicture;
-        private ImageView mLove;
-        private TextView mName, mType, mDate;
-        private RelativeLayout mRowContainer;
+        ListItemBinding binding;
+        private final RecyclerViewClickListener mListener;
 
-        public MyViewHolder(View itemView, RecyclerViewClickListener listener) {
-            super(itemView);
-            mPicture = itemView.findViewById(R.id.picture);
-            mName = itemView.findViewById(R.id.name);
-            mType = itemView.findViewById(R.id.type);
-            mLove = itemView.findViewById(R.id.love);
-            mDate = itemView.findViewById(R.id.date);
-            mRowContainer = itemView.findViewById(R.id.row_container);
+        public MyViewHolder(ListItemBinding binding, RecyclerViewClickListener listener) {
+            super(binding.getRoot());
+            this.binding = binding;
 
             mListener = listener;
-            mRowContainer.setOnClickListener(this);
-            mLove.setOnClickListener(this);
+            binding.rowContainer.setOnClickListener(this);
+            binding.love.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View v) {
             switch (v.getId()) {
                 case R.id.row_container:
-                    mListener.onRowClick(mRowContainer, getAdapterPosition());
+                    mListener.onRowClick(binding.rowContainer, getAdapterPosition());
                     break;
                 case R.id.love:
-                    mListener.onLoveClick(mLove, getAdapterPosition());
+                    mListener.onLoveClick(binding.love, getAdapterPosition());
                     break;
                 default:
                     break;
@@ -133,5 +112,4 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> implemen
 
         void onLoveClick(View view, int position);
     }
-
 }
